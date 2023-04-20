f7fd001f513d2494002ae2a37e3ab562f44066de
DP1507 <131345006+DP1507@users.noreply.github.com>
2023-04-20 18:57:03 +0530

    Update streamlit_app.py
    Added breakfast menu & three itesms

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My Parents New Healthy Diner2')
 streamlit.header('Breakfast Menu')
 streamlit.text('omega3 & blueberry oatmeal')
 streamlit.text('kale,spinach & rocket smoothie')
-streamlit.text('Hard-Boiled free Range Egg');
+streamlit.text('Hard-Boiled free Range Egg1');
