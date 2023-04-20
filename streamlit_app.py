8343da21572f22649c22289decad43112cf02f30
DP1507 <131345006+DP1507@users.noreply.github.com>
2023-04-20 18:28:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner2')
 Streamlit.header('Breakfast Menu')
 streamlit.text('omega3 & blueberry oatmeal')
 streamlit.text('kale,spinach & rocket smoothie')
