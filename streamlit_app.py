7a808d31eaa827afbaf2f700aab6cc75339ddf12
DP1507 <131345006+DP1507@users.noreply.github.com>
2023-04-20 18:51:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner2')
-Streamlit.header('Breakfast Menu')
+Streamlit.title1('Breakfast Menu')
 streamlit.text('omega3 & blueberry oatmeal')
 streamlit.text('kale,spinach & rocket smoothie')
 streamlit.text('Hard-Boiled free Range Egg');
